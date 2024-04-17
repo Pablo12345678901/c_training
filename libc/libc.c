2db46ca0350037df756fc0c97bc63b5053534915
Pablo ZAMORA <pablo.zamora@outlook.fr>
2024-04-17 03:09:29 +0200

    Create a directory to train on using the libc library.

---
+++ libc/libc.c
@@ -0,0 +1,14 @@
+#include <stdarg.h>       // va_*
+#include <stdio.h>        // printf
+#include <stdlib.h>       // calloc
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>        // time()
+#include <unistd.h>      // sleep()
+
+int main()
+{
+  printf("Hello, world !\n") ;
+  return 0 ;
+}
