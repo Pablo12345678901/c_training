6645a2b48dc25c0a21b87304e8cf077ea09a92ff
Pablo ZAMORA <pablo.zamora@outlook.fr>
2024-04-17 09:33:56 +0200

    Playing with some glibc functions to train searching through the documentation.

--- libc/libc.c
+++ libc/libc.c
@@ -10,7 +10,6 @@
 #include <stdarg.h>
 #include <stdio.h> 
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>  
 #include <unistd.h>
 /* Libc libraries */
@@ -21,6 +20,7 @@
 #include <limits.h>
 #include <pwd.h>
 #include <signal.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/times.h>
 #include <termios.h>
@@ -59,10 +59,30 @@ int main()
   game_user_id = geteuid ();
   printf("game_user_id = %d\n", game_user_id) ;
 
+  /*abort() ;/* /* Brutal end of program */
+  
   /* Deleting a file */
   char * path_tmp_file =  "/home/incognito/Desktop/dev/c_training/libc/libc.o" ;
   printf("\nDeleting the file located \"%s\".\n", path_tmp_file) ; 
   unlink(path_tmp_file) ;
+
+  /* Playing with strcpy and strcat */
+  char * ptr_dummy_first = "This is the first pointer dummy message" ;
+  int length_dummy_first = strlen(ptr_dummy_first) ;
+  /*char ptr_dummy_second[] = ", but of course it with the second dummy message, we can learn how works the function \"strcat\"" ; */
+  char * ptr_dummy_second = ", but of course it with the second dummy message, we can learn how works the function \"strcat\"" ;
+  int length_dummy_second = strlen(ptr_dummy_second) ;
+  int length_strcat_wished = length_dummy_first + length_dummy_second + 1 ;
+  char * ptr_enhanced = (char *) malloc(length_strcat_wished * sizeof(char)) ;
+  printf("") ; /* Esthetic */
+  /* strcpy copy from source to destination and replace its content. But the source keep the content too. */
+  printf("Created ptr_enhanced with memory allocation (malloc) os size %d sizeof(char) at the adress \"%p\".\n", length_strcat_wished) ; 
+  strcpy(ptr_enhanced, ptr_dummy_first) ;
+  /* strcat copy from source to destination end and remove original content. */
+  printf("After copying the value of ptr_dummy_first into it, its value is now : \n\"%s\" \nwhile the value of the ptr_dummy_first is still \n\"%s\".\n\n", ptr_enhanced, ptr_dummy_first) ;
+  strcat(ptr_enhanced, ptr_dummy_second) ;
+  printf("Now, strcat concatenante the ptr_enhanced with the second pointer and its value is now : \n\"%s\".\n", ptr_enhanced) ;
+  free(ptr_enhanced) ; /* Freeing memory */
   
   /* Showing user that everything went well until the end. */
   printf("\nEnd of script.\n") ;
