6a0cf70c879f7ab8d4d18c395b71b7216b6e78ed
Pablo ZAMORA <pablo.zamora@outlook.fr>
2024-05-14 19:25:08 +0200

    Reduce included header files to strict minimal. Demonstrate the issue with computation of float number by proving that lose of precision happends.

--- libc/types_and_number_of_bits.c
+++ libc/types_and_number_of_bits.c
@@ -1,37 +1,31 @@
 /* Macro */
-#define _POSIX_SOURCE 0
-#define _POSIX_C_SOURCE 200809L
+#define _POSIX_C_SOURCE 200809L /* If you define this macro to a value greater than or equal to
+     ‘200809L’, then the functionality from the 2008 edition of the
+     POSIX standard (IEEE Standard 1003.1-2008) is made available. */ 
 #define _LARGEFILE_SOURCE
 #define _LARGEFILE64_SOURCE
 #define _FILE_OFFSET_BITS 64
-#define _ISOC11_SOURCE
+#define _ISOC11_SOURCE /*      If this macro is defined, ISO C11 extensions to ISO C99 are
+			       included. */
 #define _GNU_SOURCE
 /* Standard libraries */
+#include <math.h> /* WARNING :
+		     'gcc' compilation with '-lm' option is required to precise that the 'libm' has to be included as it is not part of 'libc' */
 #include <stdarg.h>
-#include <stdio.h> 
-#include <stdlib.h>
-#include <time.h>  
-#include <unistd.h>
 /* Libc libraries */
-#include <assert.h> /* Asserting expression and exit if error occurs */
-#include <dirent.h>
-#include <errno.h> /* Error management */
-#include <fcntl.h>
 #include <float.h> /* As limits.h but for float numbers */
 #include <grp.h>
 #include <limits.h> /* Contains the limits of min/max values for each type */
-#include <pwd.h>
-#include <regex.h>
-#include <signal.h>
+#include <stdio.h> 
+#include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <sys/times.h>
-#include <termios.h>
 
 #define VAR0 char
 
 int main(int ARGC, char *ARGV[])
 {
+  int i ; /* For loops */
+  
   printf("\nThe value of %s is %d.\n", "CHAR_BIT", CHAR_BIT) ; /* CHAR_BIT : each type use a multiple of this number as bits width. */
   
   /* From libc doc : to get the number of bits used by a type :
@@ -84,6 +78,33 @@ int main(int ARGC, char *ARGV[])
   printf("\nAnd number of bits used for some pointer types...\n") ;
   printf("The number of bits used for the type %6s is %2u.\n", "char *", number_of_bits_used_for_type_ptr_char) ;
   printf("The number of bits used for the type %6s is %2u.\n", "int *", number_of_bits_used_for_type_ptr_int) ;
+
+  /* Stuff about float numbers */
+  printf("\nValue of some constants linked to float numbers...\n") ;
+  printf("Base of float exposant : FLT_RADIX = %d \n", FLT_RADIX) ;
+  printf("Number of digits in the mantissa : FLT_MANT_DIG = %d \n", FLT_MANT_DIG) ;
+
+  printf("\n") ; /* Esthetic */
+  printf("Demonstration of losing precision while computing with float numbers. \n") ;
+  float radix = FLT_RADIX;
+  float result_of_two_with_24_divisions = 1.0f + 1.0f \
+    / radix / radix / radix / radix / radix / radix / radix / radix / radix / radix \
+    / radix / radix / radix / radix / radix / radix / radix / radix / radix / radix \
+    / radix / radix / radix / radix ;  /* Each line contains 10 times / radix except the last one. */
+  /* float result_of_two_with_4_divisions = 1.0f + 1.0f / radix / radix / radix / radix ; */
+  float result_of_two_with_4_divisions = 1.0f + 1.0f / radix / radix / radix / radix ;
+  printf("Result of :\n1.0f + \n1.0f divided by 24 times by the number %f (that is to say, at number that tends to 0+.) \ngives the result %f \nwhile it should give a result very low near 1.0f but greater than it. \nTo demonstrate it, this is the result while dividing it only 4 times : %f. \n", radix, result_of_two_with_24_divisions, result_of_two_with_4_divisions) ;
+  printf("The error is due to the maximum number of digits (FLT_MANT_DIG = %d) within the float mantissa. \n", FLT_MANT_DIG) ;
+
+  printf("\n") ; /* Esthetic */
+
+  printf("Demonstration with print of each number...\n") ;
+  for (i=1; i<=FLT_MANT_DIG; i++)
+    { float power_from_int_to_float = (float) i ;
+      float result_for_each_step = 1.0f + (1.0f / powf(radix,power_from_int_to_float)) ;
+      printf("The result of : \n1.0f + \n1.0f / %f ^ %d \n= %f \n", i, radix, result_for_each_step) ;
+      printf("\n") ; /* Esthetic */
+    }
   
   /* Showing user that everything went well until the end. */
   printf("\nEnd of script.\n") ;
