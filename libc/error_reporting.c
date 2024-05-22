1cdc6ff5bbe12010372aa5c4189a7c39078ce4d8
Pablo ZAMORA <pablo.zamora@outlook.fr>
2024-05-22 14:22:34 +0200

    Add printing of error codes, with descriptions and messages.

--- libc/error_reporting.c
+++ libc/error_reporting.c
@@ -54,13 +54,17 @@ int main(int ARGC, char *ARGV[])
       int length_of_first_argument = strlen(ARGV[1]) + 1 ;
       char * rest_of_string_not_parsed = (char *) malloc(length_of_first_argument * sizeof(char)) ;
       long int long_int_from_string = strtol (ARGV[1], &rest_of_string_not_parsed, 0) ;
-      printf("The string %s was evaluated to the long int %ld and the non parsed rest of string \"%s\".\n", ARGV[1], long_int_from_string, rest_of_string_not_parsed) ;
+      printf("\nThe string \"%s\" was evaluated to the long int %ld and the non parsed rest of string \"%s\".\n", ARGV[1], long_int_from_string, rest_of_string_not_parsed) ;
       
       /* Force conversion from long to int */
       errno = (int) long_int_from_string ;
       
       /* Perform the assertion of errno */
-      printf("Set manually the errno to %d and then call 'assert_perror' that will print an error message (only if errno <> 0) before exiting.\n", errno) ;
+      printf("\nSet manually the errno to %d and then call 'assert_perror' that will print an error message (only if errno <> 0) before exiting.\n", errno) ;
+
+      /* The var 'program_invocation_short_name' shows the program from which came the error. */
+      printf("\nThe program from which the error came is within the variable \"%s\" which value is \"%s\". \n", "program_invocation_short_name", program_invocation_short_name) ;
+      
       /* If non zero 'errno' is provided to assert_perror, error message is show and 'Aborted' is printed on the next line. */
       assert_perror(errno) ;
     }
@@ -84,6 +88,25 @@ int main(int ARGC, char *ARGV[])
      user ID. If you have reached the limit for your user ID, ‘fork’
      will fail with ‘EAGAIN’.  *Note Creating a Process::. */
   printf("\nThe maximum number of processes that can be created with the same user ID is RLIMIT_NPROC = %d \n", RLIMIT_NPROC) ;
+
+  /* Modifying the 'errno' value to play with error functions. */
+  errno = 1 ;
+  char * error_specific = strerror(errno) ; /* WARNING : in a multithread context, use instead :
+					       -- Function: char * strerror_r (int ERRNUM, char *BUF, size_t N) */
+  printf("\nThe string specifying the error 'kind' returned by strerror(%d) with error code %d is \"%s\".\n", errno, errno, error_specific) ;
+
+  printf("\n") ; /* Esthetic */
+  perror("This customized message was added by the function 'perror' before the error returned by the strerror from the 'errno' value and can be useful to print a customized message before the error 'kind' within an error message. Such as the one of 'assert_perror(errno)' in the format 'PROGRAM: FILE:LINE-NUMBER: FUNCTION: CUSTOMIZED-MESSAGE: ERROR-KIND'") ; /* Example of message returned by 'assert_perror(errno)' :
+							  compiled-file: error_reporting.c:69: main: Unexpected error: Operation not permitted. */
+  printf("\n") ; /* Esthetic */
+  errno = 0 ; /* Re-set to clean state */
+  
+  /* Looping to show the error description for each error code known */
+  int error_code_max = 133 ; /* 240522 : Max error code = 133 */
+  for (errno=0; errno<error_code_max ; errno++)
+    { printf("Error code : %3d - Description : %15s - Message : %50s \n", errno, strerrorname_np(errno), strerror(errno)) ;
+    }
+  errno = 0 ; /* Re-set to clean state */
   
   /* Showing user that everything went well until the end. */
   printf("\nEnd of script.\n") ;
